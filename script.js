dfaeef1865011d4f4a65236e5062667fea59b2dc
QuentinGeerts <quentin.geerts@bstorm.be>
2022-02-01 20:19:08 +0100

    .

--- script.js
+++ script.js
@@ -1,68 +1,107 @@
 let pokeList = []
 
-window.onload = function () {
+const limitFilter = document.getElementById('limitFilter')
+const filterBtn = document.getElementById('filterBtn')
 
-  const POKE_API = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150'
 
-  // traduction de l'objet reçu en objet javascript
 
-  fetch(POKE_API)
-    .then(x => x.json())
-    .then(y => {
-      y.results.forEach(pokemon => {
-        pokeList.push(pokemon)
-      });
+filterBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  changeLimitFilter()
+})
 
-      generateCard()
-    })
+let limit = limitFilter.value
+
+window.onload = async function () {
+  refreshCards()
 }
 
-function generateCard() {
+async function getPokeJSON(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(pokeAPI => pokeAPI)
+}
 
-  pokeList.forEach(pokemon => {
-
-    fetch(pokemon.url)
-      .then(x => x.json())
-      .then(pokeDetails => {
-        // Création des éléments HTML
-        const divCol = document.createElement('div')
-        const divCard = document.createElement('div')
-        const imgCard = document.createElement('img')
-        const divBodyCard = document.createElement('div')
-        const titleCard = document.createElement('h5')
-        const pCard = document.createElement('p')
-
-        // Insérer les éléments dans les autres
-        divCol.appendChild(divCard)
-        divCard.appendChild(imgCard)
-        divCard.appendChild(divBodyCard)
-        divBodyCard.appendChild(titleCard)
-        divBodyCard.appendChild(pCard)
-
-        // Paramétrer les classes
-        divCard.classList.add('col-md-3', 'mb-3')
-        divCard.classList.add('card')
-        imgCard.classList.add('card-img-top')
-        divBodyCard.classList.add('card-body')
-        titleCard.classList.add('card-title')
-        pCard.classList.add('card-text')
-
-        // Paramétrer les attributs
-        imgCard.setAttribute('src', pokeDetails.sprites.front_default)
-        imgCard.setAttribute('alt', pokeDetails.name)
-
-        // Paramétrer le texte des éléments
-        titleCard.innerText = pokeDetails.id + '# ' + pokeDetails.name
-        pCard.innerText = ''
-
-        // Ajouter la card à la pokelist
-        const pokeDiv = document.getElementById('pokeDiv')
-        pokeDiv.appendChild(divCard)
-
-        console.log("Card créée");
-      })
+async function getPokeDetailsJSON(pokemon) {
+  return fetch(pokemon.url)
+    .then(x => x.json())
+    .then(pokeDetails => pokeDetails)
+}
 
 
-  });
+async function generateCard() {
 
+  for (const pokemon of pokeList) {
+
+    // Création des éléments HTML
+    const divCol = document.createElement('div')
+    const divCard = document.createElement('div')
+    const imgCard = document.createElement('img')
+    const divBodyCard = document.createElement('div')
+    const titleCard = document.createElement('h5')
+    const pCard = document.createElement('p')
+
+    // Insérer les éléments dans les autres
+    divCol.appendChild(divCard)
+    divCard.appendChild(imgCard)
+    divCard.appendChild(divBodyCard)
+    divBodyCard.appendChild(titleCard)
+    divBodyCard.appendChild(pCard)
+
+    // Paramétrer les classes
+    divCard.classList.add('col-md-3', 'mb-3')
+    divCard.classList.add('card')
+    imgCard.classList.add('card-img-top')
+    divBodyCard.classList.add('card-body')
+    titleCard.classList.add('card-title')
+    pCard.classList.add('card-text')
+
+    // Paramétrer les attributs
+    imgCard.setAttribute('src', pokemon.details.sprites.front_default)
+    imgCard.setAttribute('alt', pokemon.name)
+
+    // Paramétrer le texte des éléments
+    titleCard.innerText = pokemon.details.id + '# ' + pokemon.details.name
+    pCard.innerText = ''
+
+    // Ajouter la card à la pokelist
+    const pokeDiv = document.getElementById('pokeDiv')
+    pokeDiv.appendChild(divCard)
+  }
+}
+
+
+function changeLimitFilter() {
+  limit = limitFilter.value !== '' ? limitFilter.value : 1
+  refreshView()
+  refreshCards()
+}
+
+async function initPokeList() {
+  const POKE_API = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${limit}`
+
+  const pokeJson = await getPokeJSON(POKE_API)
+
+  limitFilter.setAttribute('max', pokeJson.count)
+  limitFilter.setAttribute('min', 1)
+  limitFilter.placeholder = 'max: ' + pokeJson.count
+
+
+  for (const pokemon of pokeJson.results) {
+    const pokeDetails = await getPokeDetailsJSON(pokemon)
+    pokeList.push({ name: pokemon.name, url: pokemon.url, details: pokeDetails })
+  }
+}
+
+async function refreshCards() {
+  await initPokeList()
+  generateCard()
+}
+
+function refreshView() {
+  const pokeDiv = document.getElementById('pokeDiv')
+  while (pokeDiv.firstChild) {
+    pokeDiv.removeChild(pokeDiv.firstChild)
+  }
+  pokeList = []
 }
